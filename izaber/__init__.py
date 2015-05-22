c9b97c94ffc2d0002ead6617c0ae5094d2856233
Aki Mimoto <aki@zaber.com>
2015-05-21 17:46:52 -0700

    fix to make sure it does not clash with flask

--- izaber/__init__.py
+++ izaber/__init__.py
@@ -4,7 +4,7 @@ import imp
 
 class IZaberFinder(object):
 
-    def find_module(self, module_name, package_path):
+    def find_module(self, module_name, package_path=None):
         elements = module_name.split('.')
         if elements[0] != 'izaber':
             return
@@ -14,14 +14,17 @@ class IZaberFinder(object):
         # izaber_extensionname
         #
         # where extensionname is all alphabetical 
-        elements[1] = "izaber_"+elements[1]
-        found = None
-        path = None
-        for e in elements[1:]:
-            found = imp.find_module(e,path)
-            path = [found[1]]
+        try:
+            elements[1] = "izaber_"+elements[1]
+            found = None
+            package_path = None
+            for e in elements[1:]:
+                found = imp.find_module(e,package_path)
+                package_path = [found[1]]
 
-        return IZaberLoader(*found)
+            return IZaberLoader(*found)
+        except ImportError:
+            return
 
 class IZaberLoader(object):
 
