03b58ca2754269c4e92dd8a0fe169dc31c2ad415
Aki Mimoto <aki@bako.ca>
2018-04-04 10:15:40 -0700

    add datetime library

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='izaber',
-      version='1.06',
+      version='1.07',
       description='Base load point for iZaber code',
       author='Aki Mimoto',
       url = 'https://github.com/zabertech/python-izaber',
-      download_url = 'https://github.com/zabertech/python-izaber/archive/1.6.tar.gz',
+      download_url = 'https://github.com/zabertech/python-izaber/archive/1.7.tar.gz',
       author_email='aki+izaber@zaber.com',
       license='MIT',
       packages=['izaber'],
@@ -16,6 +16,8 @@ setup(name='izaber',
       ],
       install_requires=[
           'PyYAML',
+          'pytz',
+          'python-dateutil',
           'appdirs',
           'Jinja2',
           'setuptools',
